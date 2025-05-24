98b661c84081b67446bdce5b1c2f1f8ec4b7e9d8
Simon Guille <simsimguille@gmail.com>
2025-05-24 17:07:43 +0200

    add constant file

--- front/src/app/home/components/auto-complete-panel/auto-complete-panel.component.ts
+++ front/src/app/home/components/auto-complete-panel/auto-complete-panel.component.ts
@@ -12,11 +12,6 @@ import { Ingredients } from '../../../../types/home';
 export class AutoCompletePanelComponent {
   @Input() newIngredients : Ingredients[] = [];
 
-  // onAddIngredients(ingredients: Ingredients[]) {
-  //   this.ingredients.push(...ingredients);
-  //   console.log("Nouvel ingrédient ajouté :", this.ingredients);
-  // }
-
   handleClick() {
     console.log("button clicked");
   }
--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { AutoCompletePanelComponent } from './components/auto-complete-panel/auto-complete-panel.component';
 import { Ingredients } from '../../types/home';
+
+const MINIMUM_INGREDIENT_LENGTH = 3;
 
 @Component({
   selector: 'app-home',
@@ -12,7 +14,7 @@ export class HomeComponent {
   newIngredients : Ingredients[] = [];
 
   handleChange(event : Event) {
-    if((event.target as HTMLInputElement).value === ''){
+    if ((event.target as HTMLInputElement).value.length < MINIMUM_INGREDIENT_LENGTH){
       this.newIngredients = [];
     } else {
       const newId = Math.floor(Math.random() * 1000);
